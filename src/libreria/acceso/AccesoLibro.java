76cb023ee2fc412e80df689ad0eb3832b7eadca6
unknown <DAM2D03@tic.ish>
2024-10-31 19:29:00 +0100

    pruebas v5

--- src/libreria/acceso/AccesoLibro.java
+++ src/libreria/acceso/AccesoLibro.java
@@ -40,22 +40,21 @@ public class AccesoLibro {
 
 		return libros;
 	}
-	
+
 	public static void escribirListaLibros(List<Libro> libros) {
 		for (Libro lib : libros) {
 			System.out.println(lib.toString());
 		}
 	}
-	
-	public static List<Libro> consultarLibrosPorNombreAutor (String nombreAutor) throws SQLException, ClassNotFoundException{
+
+	public static List<Libro> consultarLibrosPorNombreAutor(String nombreAutor)
+			throws SQLException, ClassNotFoundException {
 		List<Libro> libros = new ArrayList<>();
 		Connection conexion = null;
 		try {
 			conexion = ConfigBD.conectarseABD();
-			String codigoSQL = "SELECT l.* FROM libro as l "
-					+ "JOIN escritor as e "
-					+ "ON l.codigo_escritor = e.codigo "
-					+ "WHERE e.nombre = ? ORDER by l.agno_publicacion ASC";
+			String codigoSQL = "SELECT l.* FROM libro as l " + "JOIN escritor as e "
+					+ "ON l.codigo_escritor = e.codigo " + "WHERE e.nombre = ? ORDER by l.agno_publicacion ASC";
 			PreparedStatement sentencia = conexion.prepareStatement(codigoSQL);
 			sentencia.setString(1, nombreAutor);
 			ResultSet resultados = sentencia.executeQuery();
@@ -70,10 +69,10 @@ public class AccesoLibro {
 				Libro libro = new Libro(codigoLibro, escritor, agnoPublicacion, numeroPaginas, precio, titulo);
 				libros.add(libro);
 			}
-		}finally {
+		} finally {
 			ConfigBD.desconectar(conexion);
 		}
-		
+
 		return libros;
 	}
 
@@ -93,33 +92,70 @@ public class AccesoLibro {
 				libros.add(libro);
 			}
 		} finally {
-            ConfigBD.desconectar(conexion);
+			ConfigBD.desconectar(conexion);
 		}
-		
+
 		return libros;
 	}
-	
-	public static int insertarLibro (Libro lib) throws ClassNotFoundException, SQLException {
-		Connection conexion = null;
-        int filas = 0;
-        try {
-            conexion = ConfigBD.conectarseABD();
-            String codigoSQL = "INSERT INTO libro (codigo_escritor, titulo, agno_publicacion, numero_paginas, precio) "
-                    + "VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement sentencia = conexion.prepareStatement(codigoSQL);
-            sentencia.setInt(1, lib.getCodigoAutor());
-            sentencia.setString(2, lib.getTitulo());
-            sentencia.setInt(3, lib.getAgnoPublicacion());
-            sentencia.setInt(4, lib.getNumeroPaginas());
-            sentencia.setDouble(5, lib.getPrecio());
-            filas = sentencia.executeUpdate();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            ConfigBD.desconectar(conexion);
-        }
-        return filas;
+	public static int insertarLibro(Libro lib) throws ClassNotFoundException, SQLException {
+		Connection conexion = null;
+		int filas = 0;
+		try {
+			conexion = ConfigBD.conectarseABD();
+			String codigoSQL = "INSERT INTO libro (codigo_escritor, titulo, agno_publicacion, numero_paginas, precio) "
+					+ "VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement sentencia = conexion.prepareStatement(codigoSQL);
+			sentencia.setInt(1, lib.getCodigoAutor());
+			sentencia.setString(2, lib.getTitulo());
+			sentencia.setInt(3, lib.getAgnoPublicacion());
+			sentencia.setInt(4, lib.getNumeroPaginas());
+			sentencia.setDouble(5, lib.getPrecio());
+			filas = sentencia.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConfigBD.desconectar(conexion);
+		}
+		return filas;
 	}
-	
+
+	public static int actualizarLibroEntreFechas(int fecha1, int fecha2, double descuento)
+			throws ClassNotFoundException, SQLException {
+		Connection conexion = null;
+		int filas = 0;
+		try {
+			conexion = ConfigBD.conectarseABD();
+			String codigoSQL = "UPDATE libro SET precio = precio - ((precio * ?)/100) "
+					+ "WHERE agno_publicacion  BETWEEN ? AND ?";
+			PreparedStatement sentencia = conexion.prepareStatement(codigoSQL);
+			sentencia.setDouble(1, descuento);
+			sentencia.setInt(2, fecha1);
+			sentencia.setInt(3, fecha2);
+			filas = sentencia.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			ConfigBD.desconectar(conexion);
+		}
+		return filas;
+	}
+
+	public static int eliminarLibrosEntrePrecios(double precio1, double precio2) throws SQLException, ClassNotFoundException {
+		int filas = 0;
+		Connection conexion = null;
+		try {
+			conexion = ConfigBD.conectarseABD();
+			String codigoSQL = "DELETE FROM libro WHERE precio BETWEEN ? AND ?";
+			PreparedStatement sentencia = conexion.prepareStatement(codigoSQL);
+			sentencia.setDouble(1, precio1);
+			sentencia.setDouble(2, precio2);
+			filas = sentencia.executeUpdate();
+		}  finally {
+			ConfigBD.desconectar(conexion);
+		}
+		return filas;
+	}
+
 }
--- src/libreria/principal/PrincipalManipulacion.java
+++ src/libreria/principal/PrincipalManipulacion.java
@@ -10,109 +10,122 @@ import libreria.acceso.AccesoEscritor;
 import libreria.acceso.AccesoLibro;
 
 public class PrincipalManipulacion {
-		public static void escribirMenu() {
-			System.out.println("0) Salir");
-			System.out.println("1) Insertar un escritor en la base de datos.");
-			System.out.println("2) Actualizar el nombre, la nacionalidad, la fecha de nacimiento y la fecha de fallecimiento de un escritor de la base de datos.");
-			System.out.println("3) Eliminar un escritor de la base de datos.");
-			System.out.println("4) Insertar un libro en la base de datos.");
-			System.out.println("5) Actualizar el precio de los libros de la base de datos que se han editado "
-					+ "entre dos años de publicación con un descuento. ⋆");
-			System.out.println("6) Eliminar los libros de la base de datos que se venden entre dos precios.)");
-		}
-		
-		public static void main(String[] args) {
-			try {
-				int opcion = 0;
-				int codigoEscritor, codigoLibro, agno_publicacion;
-				int numero_paginas;
-				String nombreEscritor, nacionalidad;
-				String fechaNacimiento, fechaFallecimiento;
-				String titulo;
-				double precio;
-				Fecha fecha1, fecha2;
-				Escritor escritor = null;
-				boolean vivo;
-				List<Escritor> escritores = null;
-				do {
-					escribirMenu();
-					opcion = Teclado.leerEntero("Introduzca una opción: ");
-					switch (opcion) {
-					case 0:
-						System.out.println("Fin del programa.");
-						break;
-					case 1:
-						// Insertar un escritor en la base de datos.
-						nombreEscritor = Teclado.leerCadena("Introduzca el nombre del escritor: ");
-						nacionalidad = Teclado.leerCadena("Introduzca la nacionalidad del escritor: ");
-						fechaNacimiento = Teclado.leerCadena("Introduzca la fecha de nacimiento del escritor: ");
-						vivo = Teclado.leerBooleano("¿Está vivo el escritor?");
-						if (vivo) {
-							fechaFallecimiento = null;
-						} else {
-							fechaFallecimiento = Teclado
-									.leerCadena("Introduzca la fecha de fallecimiento del escritor: ");
-						}
-						//String nombre, String nacionalidad, int codigo, String fechaNacimiento,
-						//String fecchaFallecimiento
-						escritor = new Escritor(nombreEscritor, nacionalidad, 0, fechaNacimiento, fechaFallecimiento);
-						if (AccesoEscritor.insertarEscritor(escritor)) {
-							System.out.println("Escritor insertado correctamente.");
-						} else {
-							System.out.println("No se ha podido insertar el escritor.");
-						}
-						break;
-					case 2:
-						// Actualizar el nombre, la nacionalidad, la fecha de nacimiento y la fecha de fallecimiento de un escritor de la base de datos.
-						codigoEscritor = Teclado.leerEntero("Introduzca el código del escritor: ");
-						nombreEscritor = Teclado.leerCadena("Introduzca el nombre del escritor: ");
-						nacionalidad = Teclado.leerCadena("Introduzca la nacionalidad del escritor: ");
-						fechaNacimiento = Teclado.leerCadena("Introduzca la fecha de nacimiento del escritor: ");
-						fechaFallecimiento = Teclado.leerCadena("Introduzca la fecha de fallecimiento del escritor: ");
-						
-						escritor = new Escritor(nombreEscritor, nacionalidad, codigoEscritor, fechaNacimiento, fechaFallecimiento);
-						AccesoEscritor.actualizarEscritor(escritor);
-						break;
-					case 3:
-						// Eliminar un escritor de la base de datos.
-						codigoEscritor = Teclado.leerEntero("Introduzca el código del escritor: ");
-						escritor = new Escritor(codigoEscritor);
-						AccesoEscritor.eliminarEscritor(escritor);
-						break;
-					case 4:
-						// Insertar un libro en la base de datos.
-						codigoEscritor = Teclado.leerEntero("Introduzca el código del escritor: ");
-						escritores = AccesoEscritor.consultarTodos();
-						for (Escritor esc : escritores) {
-							System.out.println(esc.getCodigo() + " "+esc.getNombre());
-							int cod = esc.getCodigo();
-							if (cod == codigoEscritor) {
-								
-								titulo = Teclado.leerCadena("Introduzca el título del libro: ");
-								agno_publicacion = Teclado.leerEntero("Introduzca el año de publicación del libro: ");
-								numero_paginas = Teclado.leerEntero("Introduzca el número de páginas del libro: ");
-								precio = Teclado.leerReal("Introduzca el precio del libro: ");
-								//int, Escritor, int, int, double, String
-								Libro lib = new Libro(0, esc, agno_publicacion,numero_paginas, precio, titulo);
-								int filas =AccesoLibro.insertarLibro(lib);
-								String mensaje = filas > 0 ? "Libro insertado correctamente." : "No se ha podido insertar el libro.";
-								System.out.println(mensaje);
-								break;
-							} else {
-								System.out.println("El escritor no existe.");
-							}
-						}
-						
+	public static void escribirMenu() {
+		System.out.println("0) Salir");
+		System.out.println("1) Insertar un escritor en la base de datos.");
+		System.out.println(
+				"2) Actualizar el nombre, la nacionalidad, la fecha de nacimiento y la fecha de fallecimiento de un escritor de la base de datos.");
+		System.out.println("3) Eliminar un escritor de la base de datos.");
+		System.out.println("4) Insertar un libro en la base de datos.");
+		System.out.println("5) Actualizar el precio de los libros de la base de datos que se han editado "
+				+ "entre dos años de publicación con un descuento. ⋆");
+		System.out.println("6) Eliminar los libros de la base de datos que se venden entre dos precios.)");
+	}
 
-						
-						break;
+	public static void main(String[] args) {
+		try {
+			int opcion = 0;
+			int codigoEscritor, codigoLibro, agno_publicacion;
+			int numero_paginas;
+			String nombreEscritor, nacionalidad;
+			String fechaNacimiento, fechaFallecimiento;
+			String titulo;
+			double precio;
+			int fecha1, fecha2;
+			Escritor escritor = null;
+			boolean vivo;
+			List<Escritor> escritores = null;
+			do {
+				escribirMenu();
+				opcion = Teclado.leerEntero("Introduzca una opción: ");
+				switch (opcion) {
+				case 0:
+					System.out.println("Fin del programa.");
+					break;
+				case 1:
+					// Insertar un escritor en la base de datos.
+					nombreEscritor = Teclado.leerCadena("Introduzca el nombre del escritor: ");
+					nacionalidad = Teclado.leerCadena("Introduzca la nacionalidad del escritor: ");
+					fechaNacimiento = Teclado.leerCadena("Introduzca la fecha de nacimiento del escritor: ");
+					vivo = Teclado.leerBooleano("¿Está vivo el escritor?");
+					if (vivo) {
+						fechaFallecimiento = null;
+					} else {
+						fechaFallecimiento = Teclado.leerCadena("Introduzca la fecha de fallecimiento del escritor: ");
 					}
+					// String nombre, String nacionalidad, int codigo, String fechaNacimiento,
+					// String fecchaFallecimiento
+					escritor = new Escritor(nombreEscritor, nacionalidad, 0, fechaNacimiento, fechaFallecimiento);
+					if (AccesoEscritor.insertarEscritor(escritor)) {
+						System.out.println("Escritor insertado correctamente.");
+					} else {
+						System.out.println("No se ha podido insertar el escritor.");
+					}
+					break;
+				case 2:
+					// Actualizar el nombre, la nacionalidad, la fecha de nacimiento y la fecha de
+					// fallecimiento de un escritor de la base de datos.
+					codigoEscritor = Teclado.leerEntero("Introduzca el código del escritor: ");
+					nombreEscritor = Teclado.leerCadena("Introduzca el nombre del escritor: ");
+					nacionalidad = Teclado.leerCadena("Introduzca la nacionalidad del escritor: ");
+					fechaNacimiento = Teclado.leerCadena("Introduzca la fecha de nacimiento del escritor: ");
+					fechaFallecimiento = Teclado.leerCadena("Introduzca la fecha de fallecimiento del escritor: ");
+
+					escritor = new Escritor(nombreEscritor, nacionalidad, codigoEscritor, fechaNacimiento,
+							fechaFallecimiento);
+					AccesoEscritor.actualizarEscritor(escritor);
+					break;
+				case 3:
+					// Eliminar un escritor de la base de datos.
+					codigoEscritor = Teclado.leerEntero("Introduzca el código del escritor: ");
+					escritor = new Escritor(codigoEscritor);
+					AccesoEscritor.eliminarEscritor(escritor);
+					break;
+				case 4:
+					// Insertar un libro en la base de datos.
+					codigoEscritor = Teclado.leerEntero("Introduzca el código del escritor: ");
+					escritores = AccesoEscritor.consultarTodos();
+					for (Escritor esc : escritores) {
+						int cod = esc.getCodigo();
+						//int codigo, Escritor Autor, int agnoPublicacion, int numeroPaginas, double precio, String titul
+						if (cod == codigoEscritor) {
+							agno_publicacion = Teclado.leerEntero("Introduzca el año de publicación: ");
+							numero_paginas = Teclado.leerEntero("Introduzca el número de páginas: ");
+							precio = Teclado.leerReal("Introduzca el precio: ");
+							titulo = Teclado.leerCadena("Introduzca el título: ");
+							Libro libro = new Libro(0, esc, agno_publicacion, numero_paginas, precio, titulo);
+							int filas = AccesoLibro.insertarLibro(libro);
+							String mensaje = filas > 0 ? "Libro insertado correctamente." : "No se ha podido insertar el libro.";
+							System.out.println(mensaje);
+							break;
+						}
+					}
+					break;
+				  case 5:
+					  // Actualizar el precio de los libros de la base de datos que se han editado entre dos años de publicación con un descuento.
+					  fecha1 = Teclado.leerEntero("Introduzca la fecha de inicio: ");
+					  fecha2 = Teclado.leerEntero("Introduzca la fecha de fin: ");
+					  double descuento = Teclado.leerReal("Introduzca el descuento: ");
+					  int filas = AccesoLibro.actualizarLibroEntreFechas(fecha1, fecha2, descuento);
+					  String mensaje = filas > 0 ? "Se han actualizado un total de "+filas+"." : "No se ha podido actualizar ningun libro.";
+					  System.out.println(mensaje);
+					  break;
 					
-					
-				} while (opcion != 0);
-			} catch (Exception e) {
-				System.out.println("Error: " + e.getMessage());
-				e.printStackTrace();
-			}
+				case 6:
+					// Eliminar los libros de la base de datos que se venden entre dos precios.
+					double precio1 = Teclado.leerReal("Introduzca el precio mínimo: ");
+					double precio2 = Teclado.leerReal("Introduzca el precio máximo: ");
+					int filasEliminadas = AccesoLibro.eliminarLibrosEntrePrecios(precio1, precio2);
+					String mensajeEliminacion = filasEliminadas > 0
+							? "Se han eliminado un total de " + filasEliminadas + " libros."
+							: "No se ha podido eliminar ningun libro.";
+					System.out.println(mensajeEliminacion);
+					break;
+				}
+			} while (opcion != 0);
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			e.printStackTrace();
 		}
+	}
 }
