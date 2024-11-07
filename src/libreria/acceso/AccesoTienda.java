e3999c541075b99f8833a26a15d2b4d6894ff289
unknown <DAM2D03@tic.ish>
2024-11-07 19:10:01 +0100

    prueba vPerdi_La_Cuenta

--- src/libreria/acceso/AccesoTienda.java
+++ src/libreria/acceso/AccesoTienda.java
@@ -8,10 +8,10 @@ import java.sql.SQLException;
 import libreri.modelo.Tienda;
 
 public class AccesoTienda {
-	
-	//3) Consultar una tienda de la base de datos.
-	
-	public static  Tienda  consultarTienda(int codigo) throws SQLException, ClassNotFoundException {
+
+	// 3) Consultar una tienda de la base de datos.
+
+	public static Tienda consultarTienda(int codigo) throws SQLException, ClassNotFoundException {
 		Tienda tienda = null;
 		Connection conexion = null;
 		try {
@@ -31,8 +31,38 @@ public class AccesoTienda {
 		} finally {
 			ConfigBD.desconectar(conexion);
 		}
-		
-		
+
 		return tienda;
+	}
+
+	public static int[] eliminarTiendaDisponibilidades(int codigoTienda) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		Connection conexion = null;
+		int eliminaciones[] = new int[2]; // [0] -> disponibilidad, [1] -> tienda]
+		int cantidadEliminaciones = 0;
+		try {
+			conexion = ConfigBD.conectarseABD();
+			conexion.setAutoCommit(false);
+			
+			String codigoSQL1 = "DELETE FROM disponibilidad WHERE codigo_tienda = ?";
+			PreparedStatement sentencia1 = conexion.prepareStatement(codigoSQL1);
+			sentencia1.setInt(1, codigoTienda);
+			eliminaciones[0] = sentencia1.executeUpdate(); // [0] -> disponibilidad, [1] -> tienda]
+			
+			String codigoSQL2 = "DELETE FROM tienda WHERE codigo = ?";
+			PreparedStatement sentencia2 = conexion.prepareStatement(codigoSQL2);
+			sentencia2.setInt(1, codigoTienda);
+			eliminaciones[1] = sentencia2.executeUpdate(); // [0] -> disponibilidad, [1] -> tienda]
+			
+			
+			conexion.commit();
+		} catch (SQLException e) {
+			if (conexion != null) {
+				conexion.rollback();
+			}
+		} finally {
+			ConfigBD.desconectar(conexion);
+		}
+		return eliminaciones;
 	}
 }
